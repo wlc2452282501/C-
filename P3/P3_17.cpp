6f04f1d64dea7ecab34d3de7ea4f520f0ca84d73
wlc2452282501 <wlc2452282501@qq.com>
2025-12-07 15:48:22 +0800

    新增P3_17.cpp,包含学生结构体及其基本信息输出

---
+++ P3/P3_17.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+struct Student
+{
+    string name;
+    int age;
+    double score;
+};
+
+int main()
+{
+    Student stu1;
+    stu1.name = "张三";
+    stu1.age = 18;
+    stu1.score = 90.0;
+    cout << stu1.name << endl;
+    cout << stu1.age << endl;
+    cout << stu1.score << endl;
+    // cout << stu1 << endl; // 直接输出结构体变量名会报错
+    return 0;
+}
